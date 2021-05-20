f13e888da6fbbb381576b73d0188fad604af79e7
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-21 01:39:34 +0900

    CSVファイルの生成

--- read.rb
+++ read.rb
@@ -1,10 +1,19 @@
 require "csv"
+require "pry"
 
-array = CSV.read("okinawa_area.csv")
+list = CSV.read("okinawa_area.csv", headers: true).map do |row|
+  { id: row[:id].to_i, county: row["郡"], city: row["市区町村"], area: row["面積"].to_f }
+end
 
-p array
+p list.find { |data| data[:id] == 6 }
 
-puts "1行目に対応する配列"
-p array[0]
-puts "2行目に対応する配列"
-p array[1]
+p list.find { |data| data[:city] == "南大東村" }
+
+p list.find_all { |data| data[:area] >= 100 }
+
+nakagami_list = list.select { |data| data[:county] == "中頭郡" }
+p nakagami_list
+
+p nakagami_list.map { |data| data[:city] }
+
+puts nakagami_list.sum { |data| data[:area] }
